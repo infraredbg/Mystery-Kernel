2aec34b71e455cc4f0d240381bbf3a91acede16e
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2017-01-09 18:11:06 +0200

    Remove generated files someone committed without "make mrproper"
    Everything is now building into out folder for easier cleaning.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Wed Jan 4 04:39:35 EAT 2017"
-#define LINUX_COMPILE_BY "mysteryagr"
-#define LINUX_COMPILE_HOST "dell-Inspiron-1011"
-#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
